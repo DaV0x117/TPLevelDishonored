ed2ff469709b6842690c6ac6b6b0a4bdd40d35d9
LaptopDav0x <DaV0x117@users.noreply.github.com>
2019-04-05 13:47:58 +0200

    cet build cest de la merde

---
+++ Dishonored_TP_Level_Design-master/Assets/Alan Script/ChangeColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] GameObject m_withColor;
+    [SerializeField] GameObject m_withoutColor;
+
+    bool m_colorisActive = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            m_colorisActive = !m_colorisActive;
+
+            if (m_colorisActive)
+            {
+                m_withColor.SetActive(true);
+                m_withoutColor.SetActive(false);
+            }
+            else
+            {
+                m_withColor.SetActive(false);
+                m_withoutColor.SetActive(true);
+            }
+        }
+    }
+
+}
