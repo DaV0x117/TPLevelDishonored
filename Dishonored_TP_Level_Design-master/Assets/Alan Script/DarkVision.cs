2991e6d12f4d0fb7fee579d37eb7c7abf7ac6e36
LaptopDav0x <DaV0x117@users.noreply.github.com>
2019-04-05 11:58:53 +0200

    juste a rajouter le blackmarket, sinon, map finie (replacer les pnj)

---
+++ Dishonored_TP_Level_Design-master/Assets/Alan Script/DarkVision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine;
+
+public class DarkVision : MonoBehaviour
+{
+    //A mettre sur le composent MainCamera du RigibodyFPSController
+    public Camera cameraVision;
+    public Camera cameraPlayer;
+    public bool usingDarkVision = false;
+    public float visionTime = 2.0f;
+
+    private void Start()
+    {
+        cameraVision.enabled = false;
+        cameraPlayer.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (usingDarkVision == false && Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            StartCoroutine(timer());
+
+        }
+    }
+
+    IEnumerator timer()
+    {
+        //avant
+        cameraVision.enabled = true;
+        cameraPlayer.enabled = false;
+        usingDarkVision = true;
+        yield return new WaitForSeconds(visionTime);
+        cameraPlayer.enabled = true;
+        cameraVision.enabled = false;
+        usingDarkVision = false;
+        // après
+    }
+}
---
+++ Dishonored_TP_Level_Design-master/Assets/Alan Script/FallDamage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    public Transform player;
+    public float startYPos;
+    public float endYPos;
+    public float damageThreshold = 2;
+    public bool damageMe = false;
+    public bool firstCall = true;
+
+    private void Update()
+    {
+        if (!this.GetComponent<RigidbodyFirstPersonController>().Grounded)
+        {
+            if (gameObject.transform.position.y > startYPos)
+            {
+                firstCall = true;
+            }
+
+            if (firstCall)
+            {
+                startYPos = gameObject.transform.position.y;
+                firstCall = false;
+                damageMe = true;
+            }
+        }
+
+        if (this.GetComponent<RigidbodyFirstPersonController>().Grounded)
+        {
+            endYPos = gameObject.transform.position.y;
+            if (startYPos - endYPos > damageThreshold)
+            {
+                if (damageMe)
+                {
+                    player.GetComponent<Player_Health>().DamagePlayer((int)startYPos - (int)endYPos - (int)damageThreshold);
+                    damageMe = false;
+                    firstCall = true;
+                }
+            }
+        }
+    }
+}
